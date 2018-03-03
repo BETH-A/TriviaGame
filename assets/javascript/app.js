0cc2d93be8ccd0069a0e9baf41bf6e4a177b20e3
BETH-A <elizabeth78@me.com>
2018-03-03 15:38:23 -0500

    TriviaGame

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+//GLOBAL VARIABLES
+// =======================
+
+//  CREATE QUESTIONS & ANSWERS - STORE IN ARRAY
+
+var questions = [
+    {
+        questionA: "Who is Harry's nemesis?", 
+        {
+            answerA: "Professor Snape",
+            answerAImage: "assets/images/xxx.gif",
+            answerB: "Lord Voldermort",
+            answerBImage: "assets/images/xxx.gif",
+            answerC: "Dumbledore",
+            answerCImage: "assets/images/xxx.gif",
+            answerD: "Joffery",
+            answerDImage: "assets/images/xxx.gif"
+        },
+    
+        questionB: "questions b", {
+            answerA: "Professor Snape",
+            answerAImage: "assets/images/xxx.gif",
+            answerB: "Lord Voldermort",
+            answerBImage: "assets/images/xxx.gif",
+            answerC: "Dumbledore",
+            answerCImage: "assets/images/xxx.gif",
+            answerD: "Joffery",
+            answerDImage: "assets/images/xxx.gif"
+        },
+    
+    },
+    
+]
+
+// Create on-click button to start the game
+
+// Create count-down timer
