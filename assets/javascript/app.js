766972002230ae7e9c26c701408866665befb367
BETH-A <elizabeth78@me.com>
2018-03-04 16:39:10 -0500

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,186 @@
 //  CREATE QUESTIONS & ANSWERS - STORE IN ARRAY
+$(function () {
 
-var questions = [
-    {
-        question: "Who is Harry's nemesis?", 
-        index: 0,
-        answers: ["Professor Snape","Lord Voldermort","Dumbledore","Joffery"],
-        correctAnswer: "Lord Voldermort",
-        image: "assets/images/xxx.gif"
-    },
+    const TRIVIA_TIME = 30;
+	const ANSWER_TIME = 7;
 
-    {
-        question: "2nd question",
-        index: 1,
-        answers:["a","b","c","d"],
-        correctAnswer: "c",
-        image: "assets/images/question2.gif"
-    },
+	let wrong = 0;
+	let correct = 0;
+	let index = -1;
+	let result = false;
+	let countdown;
 
-    {
-        question: "3rd question",
-        index: 1,
-        answers:["a","b","c","d"],
-        correctAnswer: "c",
-        image: "assets/images/question3.gif"
+	// setInterval variables
+	let showTrivia;
+	let showAnswer;
+    let timer;
+
+    let questions = [];
+
+    questions[0] = {
+            question: "Who is Harry's nemesis?",
+            answers: ["Professor Snape", "Lord Voldermort", "Dumbledore", "Joffery"],
+            correctAnswer: "Lord Voldermort",
+            image: "assets/images/xxx.gif"
+        },
+
+    questions[1]  = {
+            question: "2nd question",
+            answers: ["a", "b", "c", "d"],
+            correctAnswer: "c",
+            image: "assets/images/question2.gif"
+        },
+
+    questions[2] = {
+            question: "3rd question",
+            index: 1,
+            answers: ["a", "b", "c", "d"],
+            correctAnswer: "c",
+            image: "assets/images/question3.gif"
+        }
+    
+
+    // Create on-clicks
+    $("#btnStart").click(loadQuestion);
+
+    // Create onclick to handle dynamically added buttons
+    $("#main-content").on("click", "button", function () {
+        if ($(this).text() === "Play Again") {
+            resetGame();
+        } else {
+            checkAnswer($(this));
+        }
+    });
+
+    // Function that loads the current question to the DOM
+    function loadQuestion() {
+        index++;	
+		countdown = TRIVIA_TIME;	
+
+		// Only play if there is another question available
+		if(index < questions.length){
+			// Clear values
+			clearInterval(showTrivia);
+			clearInterval(timer);
+			clearContent();
+
+			// Update html elements
+			$("#time").html("<p>Time remaining: " + countdown + " seconds.</p>");
+			$("#question").html("<p>" + questions[index].question + "</p>");
+			createBtns(questions[index].choices);
+
+			// Set intervals
+			showAnswer = setInterval(loadAnswer, countdown * 1000);
+			timer = setInterval(countdownTimer, 1000);
+		}
+		else {
+			endGame();
+		}
     }
-]
+    // Function updates HTML with the answer
+	function loadAnswer() {
+		
+		countdown = ANSWER_TIME;
 
-// Create count-down timer
-var count=30;
+		// Set result if user didn't answer and time ran out
+		if (result === false){
+			result = "No answer chosen."
+		}
+		// Clear values
+		clearInterval(showAnswer);
+		clearInterval(timer);
+		$("#time").empty();
 
-var counter=setInterval(timer, 1000); 
+		//Update html elements
+		$("#time").html("<p>Time remaining: " + countdown + " seconds.</p>");
+		$("#question").html("<p>" + result + "</p>");
+		$("#answers").html("<b>" + questions[index].answer + "</b> - " + questions[index].fact);
 
-function timer()
-{
-  count=count-1;
-  if (count <= 0)
-  {
-     clearInterval(counter);
-     //counter ended, do something here
-     return;
-  }
-}
-// Create on-click button to start the game
+		// reset result in case timer ran out
+		result = false;
+
+		// Set intervals
+		timer = setInterval(countdownTimer, 1000);
+		showTrivia = setInterval(loadQuestion, countdown * 1000);
+	}
+
+	// Function creates and places the buttons for multiple choice onto HTML
+	function createBtns(arr) {
+		for(var i = 0; i < arr.length; i++) {
+			var btn = $("<button>");
+			btn.addClass("btn btn-warning");
+			btn.addClass("choice");
+			btn.attr("type", "button");
+			btn.val(i);
+			btn.html(questions[index].choices[i]);
+			$("#answers").append(btn);
+		}
+	}
+
+	// Function determines if user guessed correct answer	
+	function checkAnswer(obj) {
+		
+		// Determine if correct answer
+		if(obj.text() === questions[index].answer){
+			correct++;
+			result = "Correct!";
+		}
+		else {
+			wrong++;
+			result = "Incorrect!";
+		}
+		loadAnswer();
+    }
+})
+
+    // Function that shows the timer
+    function countdownTimer() {
+        countdown--;	
+        $("#time").html("<p>Time remaining: " + countdown + " seconds.</p>");
+    }
+
+    // Function to view end game results
+    function endGame() {
+
+        // Stop intervals
+        clearInterval(showTrivia);
+        clearInterval(showAnswer);
+        clearInterval(timer);	
+        
+        // Set variables
+        let unanswered = questions.length - (correct + wrong);
+        let stats = "<p>Correct: " + correct + "</p>" +
+                    "<p>Incorrect: " + wrong + "</p>" +
+                    "<p>Unanswered: " + unanswered + "</p>"
+        ;
+        let btn = "<button class='btn btn-danger' type='button' id='btnRestart'>Play Again</button>"
+
+        // Update HTML
+        clearContent();
+        $("#time").html("<u><h3>Final Score</h3></u>");
+        $("#question").html(stats);
+        $("#answers").html(btn);
+    }
+
+    // Function that resets variables for game replay
+    function resetGame() {
+        
+        // reset varaibles
+        wrong = 0;
+        correct = 0;
+        index = -1;
+        
+        // Update HTML
+        clearContent();
+
+        // Restart game
+        loadQuestion();
+    }
+
+    // Function that clears the main-content id of HTML
+    function clearContent() {
+        $("#time").empty();
+        $("#question").empty();
+        $("#answers").empty();
+    }
 
