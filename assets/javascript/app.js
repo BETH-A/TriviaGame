321246535d40f6ec2fb35612acf933e5f92d0498
BETH-A <elizabeth78@me.com>
2018-03-04 15:37:37 -0500

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,45 @@
-//GLOBAL VARIABLES
-// =======================
-
 //  CREATE QUESTIONS & ANSWERS - STORE IN ARRAY
 
 var questions = [
     {
-        questionA: "Who is Harry's nemesis?", 
-        {
-            answerA: "Professor Snape",
-            answerAImage: "assets/images/xxx.gif",
-            answerB: "Lord Voldermort",
-            answerBImage: "assets/images/xxx.gif",
-            answerC: "Dumbledore",
-            answerCImage: "assets/images/xxx.gif",
-            answerD: "Joffery",
-            answerDImage: "assets/images/xxx.gif"
-        },
-    
-        questionB: "questions b", {
-            answerA: "Professor Snape",
-            answerAImage: "assets/images/xxx.gif",
-            answerB: "Lord Voldermort",
-            answerBImage: "assets/images/xxx.gif",
-            answerC: "Dumbledore",
-            answerCImage: "assets/images/xxx.gif",
-            answerD: "Joffery",
-            answerDImage: "assets/images/xxx.gif"
-        },
-    
+        question: "Who is Harry's nemesis?", 
+        index: 0,
+        answers: ["Professor Snape","Lord Voldermort","Dumbledore","Joffery"],
+        correctAnswer: "Lord Voldermort",
+        image: "assets/images/xxx.gif"
     },
-    
+
+    {
+        question: "2nd question",
+        index: 1,
+        answers:["a","b","c","d"],
+        correctAnswer: "c",
+        image: "assets/images/question2.gif"
+    },
+
+    {
+        question: "3rd question",
+        index: 1,
+        answers:["a","b","c","d"],
+        correctAnswer: "c",
+        image: "assets/images/question3.gif"
+    }
 ]
 
+// Create count-down timer
+var count=30;
+
+var counter=setInterval(timer, 1000); 
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     //counter ended, do something here
+     return;
+  }
+}
 // Create on-click button to start the game
 
-// Create count-down timer
