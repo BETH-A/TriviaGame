237b166448987b1e631a363747bb1512c2d60683
BETH-A <elizabeth78@me.com>
2018-03-06 15:21:56 -0500

    TriviaGame

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,67 @@
 $(function () {
 
-    const TRIVIA_TIME = 30;
-	const ANSWER_TIME = 7;
+    // const TRIVIA_TIME = 30;
+	// const ANSWER_TIME = 7;
 
-	let wrong = 0;
-	let correct = 0;
-	let index = -1;
-	let result = false;
-	let countdown;
+	var wrong = 0;
+	var correct = 0;
+	var index = -1;
+	var result = false;
+    var countdown = 5;
 
 	// setInterval variables
-	let showTrivia;
-	let showAnswer;
-    let timer;
+	var showTrivia;
+	// let showAnswer;
+    var timer;
 
 //  CREATE QUESTIONS & choices - STORE IN ARRAY
-    var questions = [];
-
-    questions[0] = {
+    var questions = [
+        { 
             question: "Who is Harry's nemesis?",
             choices: ["Professor Snape", "Lord Voldermort", "Professor Dumbledore", "Draco Malfoy"],
             answer: "Lord Voldermort",
             image: "assets/images/voldermort.jpg"
         },
 
-    questions[1]  = {
+     {
             question: "What is Harry's patronus?",
             choices: ["Doe", "Stag", "Wolf", "Horse"],
             answer: "Stag",
             image: "assets/images/stag.jpg"
         },
 
-    questions[2] = {
+     {
             question: "What potion did Harry take to get Slughorn's memories?",
             choices: ["Felix Felicis", "Polyjuice Potion", "Alihotsy Draught", "Essense of Dittany"],
             answer: "Felix Felicis",
             image: "assets/images/felix-felicis.jpg"
-        }
+        },
 
-        questions[3] = {
+         {
             question: "What did Dumbledore leave for Hermonie in is will?",
             choices: ["The Luminator", "The Sword of Griffendor", "The Snitch", "The Tales of Beedle the Bard"],
             answer: "The Tales of Beelde the Bard",
             image: "assets/images/book.jpg"
-        }
+        },
     
-        questions[4] = {
+         {
             question: "How many kids are in the Weasley family?",
             choices: ["6", "7", "8", "9"],
             answer: "7",
             image: "assets/images/kids.png"
-        }
+        },
 
-        questions[5] = {
+         {
             question: "What color are the flames that came out of the Goblet of Fire?",
             choices: ["Yellow", "Orange", "Blue", "Green"],
             answer: "Blue",
             image: "assets/images/goblet-fire.jpg"
-        }
+        }]
 
     // Create on-clicks
-    $("#btnStart").click(loadQuestion);
+    $("#btnStart").on("click", function(){
+        loadQuestion();
+    });
 
     // Create onclick to handle dynamically added buttons
     $("#main-content").on("click", "button", function () {
@@ -74,7 +75,8 @@ $(function () {
     // Function that loads the current question to the DOM
     function loadQuestion() {
         index++;	
-		countdown = TRIVIA_TIME;	
+		countdown = 5;	
+        console.log(countdown);
 
 		// Only play if there is another question available
 		if(index < questions.length){
@@ -86,10 +88,10 @@ $(function () {
 			// Update html elements
 			$("#time").html("<p>Time remaining: " + countdown + " seconds.</p>");
 			$("#question").html("<p>" + questions[index].question + "</p>");
-			createBtns(questions[index].choices);
+			createBtns();
 
 			// Set intervals
-			showAnswer = setInterval(loadAnswer, countdown * 1000);
+			// showAnswer = setInterval(loadAnswer, countdown * 1000);
 			timer = setInterval(countdownTimer, 1000);
 		}
 		else {
@@ -99,7 +101,7 @@ $(function () {
     // Function updates HTML with the answer
 	function loadAnswer() {
 		
-		countdown = ANSWER_TIME;
+		// countdown = ANSWER_TIME;
 
 		// Set result if user didn't answer and time ran out
 		if (result === false){
@@ -113,25 +115,27 @@ $(function () {
 		//Update html elements
 		$("#time").html("<p>Time remaining: " + countdown + " seconds.</p>");
 		$("#question").html("<p>" + result + "</p>");
-		$("#choices").html("<b>" + questions[index].answer + "</b> - " + questions[index].fact);
+		// $("#choices").html("<b>" + questions[index].answer + "</b> - " + questions[index].fact);
 
 		// reset result in case timer ran out
 		result = false;
 
 		// Set intervals
 		timer = setInterval(countdownTimer, 1000);
-		showTrivia = setInterval(loadQuestion, countdown * 1000);
+		// showTrivia = setInterval(loadQuestion, countdown * 1000);
 	}
 
 	// Function creates and places the buttons for multiple choice onto HTML
-	function createBtns(questions) {
-		for(var i = 0; i < questions.length; i++) {
-			var btn = $("<button>");
+	function createBtns() {
+        console.log("hi");
+		for(var i = 0; i < 4; i++) {
+            var btn = $("<button>");
 			btn.addClass("btn btn-warning");
 			btn.addClass("choice");
 			btn.attr("type", "button");
 			btn.val(i);
-			btn.html(questions[index].choices[i]);
+			btn.text(questions[index].choices[i]);
+            console.log("btn");
 			$("#choices").append(btn);
 		}
 	}
@@ -148,14 +152,17 @@ $(function () {
 			wrong++;
 			result = "Incorrect!";
 		}
-		loadAnswer();
+		loadQuestion();
     }
-})
 
     // Function that shows the timer
     function countdownTimer() {
-        countdown--;	
-        $("#time").html("<p>Time remaining: " + countdown + " seconds.</p>");
+        if (countdown > 0) 	{
+        countdown = countdown-1;
+        $("#time").html("<p>Time remaining: " + countdown + " seconds.</p>");      
+        }
+        else {loadQuestion() 
+        };
     }
 
     // Function to view end game results
@@ -163,14 +170,14 @@ $(function () {
 
         // Stop intervals
         clearInterval(showTrivia);
-        clearInterval(showAnswer);
+        // clearInterval(showAnswer);
         clearInterval(timer);	
         
         // Set variables
         let unanswered = questions.length - (correct + wrong);
-        let stats = "<p>Correct: " + correct + "</p>" +
-                    "<p>Incorrect: " + wrong + "</p>" +
-                    "<p>Unanswered: " + unanswered + "</p>"
+        let stats = "<p>Correct: " + correct + "</p> <br></br>" +
+                    "<p>Incorrect: " + wrong + "</p>  <br></br>" +
+                    "<p>Unanswered: " + unanswered + "</p>  <br></br>"
         ;
         let btn = "<button class='btn btn-danger' type='button' id='btnRestart'>Play Again</button>"
 
@@ -202,3 +209,4 @@ $(function () {
         $("#question").empty();
         $("#choices").empty();
     }
+})
